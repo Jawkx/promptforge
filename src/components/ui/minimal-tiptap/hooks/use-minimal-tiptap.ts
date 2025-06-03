57975ab7d8583ce681d63f7fe37cbec884c2d53d
jaw <jawkaixuan99@gmail.com>
2025-06-03 19:08:34 +0800

    feat: remove tip tap image adding.md

--- src/components/ui/minimal-tiptap/hooks/use-minimal-tiptap.ts
+++ src/components/ui/minimal-tiptap/hooks/use-minimal-tiptap.ts
@@ -9,19 +9,16 @@ import { Underline } from "@tiptap/extension-underline"
 import { TextStyle } from "@tiptap/extension-text-style"
 import {
   Link,
-  Image,
   HorizontalRule,
   CodeBlockLowlight,
   Selection,
   Color,
   UnsetAllMarks,
   ResetMarksOnEnter,
-  FileHandler,
 } from "../extensions"
 import { cn } from "@/lib/utils"
-import { fileToBase64, getOutput, randomId } from "../utils"
+import { getOutput } from "../utils"
 import { useThrottle } from "../hooks/use-throttle"
-import { toast } from "sonner"
 
 export interface UseMinimalTiptapEditorProps extends UseEditorOptions {
   value?: Content
@@ -31,143 +28,37 @@ export interface UseMinimalTiptapEditorProps extends UseEditorOptions {
   throttleDelay?: number
   onUpdate?: (content: Content) => void
   onBlur?: (content: Content) => void
-  uploader?: (file: File) => Promise<string>
-}
-
-async function fakeuploader(file: File): Promise<string> {
-  // NOTE: This is a fake upload function. Replace this with your own upload logic.
-  // This function should return the uploaded image URL.
-
-  // wait 3s to simulate upload
-  await new Promise((resolve) => setTimeout(resolve, 3000))
-
-  const src = await fileToBase64(file)
-
-  return src
 }
 
 const createExtensions = ({
   placeholder,
-  uploader,
-}: {
-  placeholder: string
-  uploader?: (file: File) => Promise<string>
-}) => [
-  StarterKit.configure({
-    horizontalRule: false,
-    codeBlock: false,
-    paragraph: { HTMLAttributes: { class: "text-node" } },
-    heading: { HTMLAttributes: { class: "heading-node" } },
-    blockquote: { HTMLAttributes: { class: "block-node" } },
-    bulletList: { HTMLAttributes: { class: "list-node" } },
-    orderedList: { HTMLAttributes: { class: "list-node" } },
-    code: { HTMLAttributes: { class: "inline", spellcheck: "false" } },
-    dropcursor: { width: 2, class: "ProseMirror-dropcursor border" },
-  }),
-  Link,
-  Underline,
-  Image.configure({
-    allowedMimeTypes: ["image/*"],
-    maxFileSize: 5 * 1024 * 1024,
-    allowBase64: true,
-    uploadFn: async (file) => {
-      return uploader ? await uploader(file) : await fakeuploader(file)
-    },
-    onToggle(editor, files, pos) {
-      editor.commands.insertContentAt(
-        pos,
-        files.map((image) => {
-          const blobUrl = URL.createObjectURL(image)
-          const id = randomId()
-
-          return {
-            type: "image",
-            attrs: {
-              id,
-              src: blobUrl,
-              alt: image.name,
-              title: image.name,
-              fileName: image.name,
-            },
-          }
-        })
-      )
-    },
-    onImageRemoved({ id, src }) {
-      console.log("Image removed", { id, src })
-    },
-    onValidationError(errors) {
-      errors.forEach((error) => {
-        toast.error("Image validation error", {
-          position: "bottom-right",
-          description: error.reason,
-        })
-      })
-    },
-    onActionSuccess({ action }) {
-      const mapping = {
-        copyImage: "Copy Image",
-        copyLink: "Copy Link",
-        download: "Download",
-      }
-      toast.success(mapping[action], {
-        position: "bottom-right",
-        description: "Image action success",
-      })
-    },
-    onActionError(error, { action }) {
-      const mapping = {
-        copyImage: "Copy Image",
-        copyLink: "Copy Link",
-        download: "Download",
-      }
-      toast.error(`Failed to ${mapping[action]}`, {
-        position: "bottom-right",
-        description: error.message,
-      })
-    },
-  }),
-  FileHandler.configure({
-    allowBase64: true,
-    allowedMimeTypes: ["image/*"],
-    maxFileSize: 5 * 1024 * 1024,
-    onDrop: (editor, files, pos) => {
-      files.forEach(async (file) => {
-        const src = await fileToBase64(file)
-        editor.commands.insertContentAt(pos, {
-          type: "image",
-          attrs: { src },
-        })
-      })
-    },
-    onPaste: (editor, files) => {
-      files.forEach(async (file) => {
-        const src = await fileToBase64(file)
-        editor.commands.insertContent({
-          type: "image",
-          attrs: { src },
-        })
-      })
-    },
-    onValidationError: (errors) => {
-      errors.forEach((error) => {
-        toast.error("Image validation error", {
-          position: "bottom-right",
-          description: error.reason,
-        })
-      })
-    },
-  }),
-  Color,
-  TextStyle,
-  Selection,
-  Typography,
-  UnsetAllMarks,
-  HorizontalRule,
-  ResetMarksOnEnter,
-  CodeBlockLowlight,
-  Placeholder.configure({ placeholder: () => placeholder }),
-]
+}:
+  {
+    placeholder: string
+  }) => [
+    StarterKit.configure({
+      horizontalRule: false,
+      codeBlock: false,
+      paragraph: { HTMLAttributes: { class: "text-node" } },
+      heading: { HTMLAttributes: { class: "heading-node" } },
+      blockquote: { HTMLAttributes: { class: "block-node" } },
+      bulletList: { HTMLAttributes: { class: "list-node" } },
+      orderedList: { HTMLAttributes: { class: "list-node" } },
+      code: { HTMLAttributes: { class: "inline", spellcheck: "false" } },
+      dropcursor: { width: 2, class: "ProseMirror-dropcursor border" },
+    }),
+    Link,
+    Underline,
+    Color,
+    TextStyle,
+    Selection,
+    Typography,
+    UnsetAllMarks,
+    HorizontalRule,
+    ResetMarksOnEnter,
+    CodeBlockLowlight,
+    Placeholder.configure({ placeholder: () => placeholder }),
+  ]
 
 export const useMinimalTiptapEditor = ({
   value,
@@ -177,7 +68,6 @@ export const useMinimalTiptapEditor = ({
   throttleDelay = 0,
   onUpdate,
   onBlur,
-  uploader,
   ...props
 }: UseMinimalTiptapEditorProps) => {
   const throttledSetValue = useThrottle(
@@ -205,7 +95,7 @@ export const useMinimalTiptapEditor = ({
   )
 
   const editor = useEditor({
-    extensions: createExtensions({ placeholder, uploader }),
+    extensions: createExtensions({ placeholder }),
     editorProps: {
       attributes: {
         autocomplete: "off",
