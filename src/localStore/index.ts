a4a044cf0a2f2309def2119e32e83ddd4aab3bbb
jaw <jawkaixuan99@gmail.com>
2025-06-13 10:16:55 +0800

    feat: add use local store

--- src/localStore/index.ts
+++ src/localStore/index.ts
@@ -2,23 +2,39 @@ import { Context } from "@/types";
 import { create } from "zustand";
 
 interface LocalStoreState {
+  promptInput: string;
+  setPromptInput: (promptInput: string) => void;
+
   selectedContext: Context[];
   addSelectedContext: (context: Context) => void;
-  removeSelectedContext: (contextId: string) => void;
+  removeSelectedContexts: (contextIds: string[]) => void;
+  updateSelectedContext: (context: Context) => void;
 }
 
 export const useLocalStore = create<LocalStoreState>((set) => ({
+  // Prompt Input
+  promptInput: "",
+  setPromptInput: (promptInput) => set({ promptInput }),
+
+  // Selected Contexts
   selectedContext: [],
   addSelectedContext: (context: Context) =>
     set((state) => ({
       ...state,
       selectedContext: [...state.selectedContext, context],
     })),
-  removeSelectedContext: (contextId: string) =>
+  removeSelectedContexts: (contextIds: string[]) =>
     set((state) => ({
       ...state,
       selectedContext: state.selectedContext.filter(
-        (context) => context.id !== contextId,
+        (c) => !contextIds.includes(c.id),
+      ),
+    })),
+  updateSelectedContext: (context: Context) =>
+    set((state) => ({
+      ...state,
+      selectedContext: state.selectedContext.map((c) =>
+        c.id === context.id ? context : c,
       ),
     })),
 }));
