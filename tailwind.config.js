934e3e46d1148012a22ce535870cbc7b3fa105be
jaw <jawkaixuan99@gmail.com>
2025-06-21 22:59:25 +0800

    feat: add custom bounce

--- tailwind.config.js
+++ tailwind.config.js
@@ -75,10 +75,21 @@ module.exports = {
             height: "0",
           },
         },
+        'bounce-sm': {
+          '0%, 100%': {
+            transform: 'translateY(-5%)', // Smaller bounce, e.g., 5%
+            animationTimingFunction: 'cubic-bezier(0.8, 0, 1, 1)',
+          },
+          '50%': {
+            transform: 'translateY(0)',
+            animationTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)',
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "bounce-sm": "bounce-sm 1s infinite"
       },
     },
   },
