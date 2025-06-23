1b40483033bb5d8d614a9864b44f52689b7d4eb3
jaw <jawkaixuan99@gmail.com>
2025-06-23 23:28:25 +0800

    feat: add firebase auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import { Toaster } from "./components/ui/sonner";
 import Editor from "./screens/Editor";
+import { useEffect } from "react";
+import { signInAnonymously, onAuthStateChanged } from "firebase/auth";
+import { auth } from "@/lib/firebase";
 
 function App() {
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log("Firebase Anonymous User UID:", user.uid);
+      } else {
+        signInAnonymously(auth).catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.error(`Firebase auth error: ${errorCode}`, errorMessage);
+        });
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
       <Editor />
