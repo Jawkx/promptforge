5c0848aa4b9f9d01a49ea019a744e53cc5239313
jaw <jawkaixuan99@gmail.com>
2025-07-14 22:59:08 +0800

    feat: sync user store

--- src/livestore/user.worker.ts
+++ src/livestore/user.worker.ts
@@ -1,6 +1,10 @@
+import { makeCfSync } from "@livestore/sync-cf";
 import { makeWorker } from "@livestore/adapter-web/worker";
 import { userSchema } from "./user-store/schema.ts";
 
+const url = "http://localhost:8787";
+
 makeWorker({
   schema: userSchema,
+  sync: { backend: makeCfSync({ url }) },
 });
