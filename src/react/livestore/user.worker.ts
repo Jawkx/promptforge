1cc218da8f495a86d6d3c8cd865bf5327213c24a
jaw <jawkaixuan99@gmail.com>
2025-07-17 17:36:59 +0800

    feat: SHIT WORKS MFKER

--- src/react/livestore/user.worker.ts
+++ src/react/livestore/user.worker.ts
@@ -2,7 +2,7 @@ import { makeCfSync } from "@livestore/sync-cf";
 import { makeWorker } from "@livestore/adapter-web/worker";
 import { userSchema } from "./user-store/schema.ts";
 
-const url = "http://localhost:8787";
+const url = "http://localhost:5173";
 
 makeWorker({
   schema: userSchema,
