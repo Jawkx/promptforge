5be3be771c7618abd88e456052efcb1b4c3723a2
jaw <jawkaixuan99@gmail.com>
2025-07-18 08:26:29 +0800

    feat: shit works

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,19 @@ import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
-import { livestoreDevtoolsPlugin } from '@livestore/devtools-vite'
+import { livestoreDevtoolsPlugin } from "@livestore/devtools-vite";
 
 const isProdBuild = process.env.NODE_ENV === "production";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), cloudflare(), livestoreDevtoolsPlugin({ schemaPath: 'src/react/livestore/user-store/schema.ts' })],
+  plugins: [
+    react(),
+    cloudflare(),
+    livestoreDevtoolsPlugin({
+      schemaPath: "src/react/livestore/user-store/schema.ts",
+    }),
+  ],
   worker: isProdBuild ? { format: "es" } : undefined,
   resolve: {
     alias: {
